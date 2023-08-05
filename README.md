a1b5c45367033617d143353a634af5d0956ed41f
Joel <joellongaresjr@gmail.com>
2023-08-04 20:49:52 -0700

    added notes, going to finalize readme

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # track-my-employees
+## Description 
+Welcome to "NoteQuest"! This is a simple note-taking application that will allow users to create, save, and delete notes. Users will have the opportunity to stores these notes and access them at any given moment! 
+
+Click on the delployed link from Heroku to embark on your Quest on Note-taking. 
+
+[Heroku.link](https://afternoon-stream-77923-18557e5e2777.herokuapp.com/)
+
+
+
+![Quest.logo](./images/QUEST%20PICUTRE.png)
+
+
+
+## Technology Used
+* Node.js
+* Express
+* FS (File System)
+* UUID npm package
+* Disclaimer: all these technologies/packages must be installed within your CLI before you can experience this application 
+
+## Installation
+1. Clone repo 
+2. Navigate in your CLI to acces
+3. Install npm & all the necessary pacakges before deployment
+
+## Code Highlights
+
+Here is a snippet for when I used the turnary way of writing code within my fs.writeFile
+I also broadcast the npm package (uuid) in this function
+![Turnary/uuid.sample](./images/Turnary%20Example.png)
+
+
+## Learning Points
+
+* UUID an Npm built in package allowed me to execute this assignment. Using this package allowed me give each note a unique id and calling on that particular note that the user inputs to our database (db.json)
+* (Turnary) using this method instead of (if/else) by default makes everything look prettier and saving space within our code. My tutor from the Berkley Coding Extension shared this valuable insight with me, and I'm grateful for the knowledge they imparted. Implementing this approach proves advantageous in the long run, as it minimizes code space usage. In real-world scenarios, optimizing code space becomes crucial, especially when dealing with databases and constant information being passed on. 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-const cTable = require("console.table");
+const cTable = require("console.table"); 
 
 const db = mysql.createConnection(
   {
@@ -11,7 +11,7 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employees_db database.`)
 );
-
+// function helps us establish a connection to mysql which help us initiate 'promptUser' to our user!
 db.connect((err) => {
   if (err) {
     console.error(err);
@@ -21,6 +21,7 @@ db.connect((err) => {
   promptUser();
 });
 
+// using npm 'inquirer' to prompt questions to our user, 
 const promptUser = () => {
   inquirer
     .prompt([
@@ -41,8 +42,8 @@ const promptUser = () => {
       },
     ])
     .then((answers) => {
-      const { choice } = answers;
-
+      const { choice } = answers; // extracting our users choices from answers object
+// applying a switch statment to help us determine the actions required to assist the user through our prompt
       switch (choice) {
         case "View all departments":
           viewAllDepartments();
@@ -84,28 +85,28 @@ const promptUser = () => {
       }
     });
 };
-
+// Function that allows users to see all Depts
 const viewAllDepartments = async () => {
   try {
-    const [results] = await db.promise().query("SELECT * from department;");
+    const [results] = await db.promise().query("SELECT * from department;"); // query select all rows from dept table
 
     console.log("");
-    console.log("***** LIST OF YOUR DEPARTMENTS *****");
+    console.log("***** LIST OF YOUR DEPARTMENTS *****"); // cute way of displaying our tables to the user (kawaii desu ne)
     console.log("");
-    console.table(results);
+    console.table(results); 
   } catch (err) {
     throw err;
   }
-  promptUser();
+  promptUser(); // mainlly used throughout our code but this prompts the user back to the main menu 
 };
-
+// Function that allows users to see all Roles
 const viewAllRoles = async () => {
   try {
     const [results] = await db
       .promise()
-      .query(
+      .query( // I hated this portion of the assignment because this query is by far too complex, but that's my fault for making my data for my tables to complex)
         "SELECT role.department_id AS id, role.title, role.salary, department.name AS department FROM role INNER JOIN department ON role.department_id = department.id;"
-      );
+      );// initially we use 'Inner Join' to create a relationship between deparment and role table, displaying our columns according, ex(role.department_id as id) 
     console.log("****** LIST OF YOUR ROLES ******");
     console.table(results);
   } catch (err) {
@@ -118,9 +119,9 @@ const viewAllEmployees = async () => {
   try {
     const [results] = await db
       .promise()
-      .query(
+      .query(// this most complex query of them all!! added a new sql method 'CONCAT' which allows to associate first & last name of the manager to associate with their respected employee
         "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id;"
-      );
+      ); // overall this query gives us the access of infomration about employees and their specific roles within the company and identifying all departments, salary, roles and corresponding managers of those employees
     console.log("***** List OF YOUR EMPLOYEES ******");
     console.table(results);
   } catch (err) {
@@ -128,30 +129,30 @@ const viewAllEmployees = async () => {
   }
   promptUser();
 };
-
+// function that allows us to add a new dept to our database
 const addNewDepartment = async () => {
   try {
     const answer = await inquirer.prompt([
       {
         type: "input",
         name: "departmentName",
-        message: "What is the name of the department you wish to add?",
+        message: "What is the name of the department you wish to add?", // prompting user of what dept they wish to add
       },
     ]);
-    const departmentName = answer.departmentName;
+    const departmentName = answer.departmentName; // once the user provides us their information of the new dept, the 'answer' object is then extracted and stored in deptname variable
     await db
-      .promise()
-      .query(`INSERT INTO department (name) VALUES ('${departmentName}')`);
+      .promise() 
+      .query(`INSERT INTO department (name) VALUES ('${departmentName}')`); // once promise has been fulfilled, we then insert the new department (name) into our database 'VALUES $deptname'
     console.log(`${departmentName} added to department list`);
   } catch (err) {
     console.error(err);
   }
   promptUser();
 };
-
+// function that allows users to add a new role 
 const addNewRole = async () => {
-  try {
-    const answer = await inquirer.prompt([
+  try { // prompt our user for role & salary 
+    const answer = await inquirer.prompt([ 
       {
         type: "input",
         name: "roleName",
@@ -163,18 +164,19 @@ const addNewRole = async () => {
         message: "What is the salary of this role?",
       },
     ]);
-
+// extracrting answers the user provided (rolename & salary)
     const roleName = answer.roleName;
     const salary = answer.salary;
-
+// fetch all depts from the data to allow the user to choose which dept the new role will belong to 
     const [departments] = await db
       .promise()
       .query("SELECT name, id FROM department");
-
+// mapping our departments into an array of choices (giving that dept a name, and new coressponding id)
     const departmentChoices = departments.map(({ name, id }) => ({
       name: name,
       value: id,
-    }));
+    })); 
+// prompting the user to select where the new role will be selected within the company (hence, deptchoice)
     const deptChoice = await inquirer.prompt([
       {
         type: "list",
@@ -183,9 +185,12 @@ const addNewRole = async () => {
         choices: departmentChoices,
       },
     ]);
-    const departmentId = deptChoice.departmentId;
+    const departmentId = deptChoice.departmentId; // extracting the department ID selected by the user!
+// SQL template to insert the new role into the data base
     const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+// awaiting a promise to insert a new role into the data base from the provided values (role name, salary, & coreresponding deptid)
     await db.promise().query(sql, [roleName, salary, departmentId]);
+
     console.log(`Added ${roleName} to the company!`);
   } catch (err) {
     console.error(err);
@@ -193,17 +198,19 @@ const addNewRole = async () => {
   promptUser();
 };
 
+// Function to that updates our employee table
 const updateEmployee = async () => {
   try {
-    const [employees] = await db
+    const [employees] = await db // fetch a list of employee from our database
       .promise()
       .query("SELECT id, first_name, last_name FROM employee");
 
+      // creating an array of choices for the user by the corresponding name/id
     const employeeChoices = employees.map((employee) => ({
       name: `${employee.first_name} ${employee.last_name}`,
       value: employee.id,
     }));
-
+// Prompt the user to select the employee they wish to update
     const selectedEmployeeAnswer = await inquirer.prompt([
       {
         type: "list",
@@ -211,17 +218,17 @@ const updateEmployee = async () => {
         message: "Select the employee you want to update:",
         choices: employeeChoices,
       },
-    ]);
-
+    ]); 
+// extracting the id of the selected employee from which the user provided (once the promise has been fulfilled)
     const { selectedEmployeeId } = selectedEmployeeAnswer;
-
+// fetching the list of roles from our database 
     const [roles] = await db.promise().query("SELECT id, title FROM role");
 
     const roleChoices = roles.map((role) => ({
       name: role.title,
       value: role.id,
     }));
-
+// Prompting our user to select the new role from the employee they selected
     const selectedRoleAnswer = await inquirer.prompt([
       {
         type: "list",
@@ -230,9 +237,9 @@ const updateEmployee = async () => {
         choices: roleChoices,
       },
     ]);
-
+// extracting the id of the selected role from the user's answer
     const { selectedRoleId } = selectedRoleAnswer;
-
+// once we receive this promise we then update the role of the employee in our database
     await db
       .promise()
       .query("UPDATE employee SET role_id = ? WHERE id = ?", [
